3388bf1b768b57ba9897f4102e383283d5add771
Samoht07 <341737@talnet.nl>
2019-11-15 15:47:06 +0100

    Uitwerking van de exercise

--- hello.php
+++ hello.php
@@ -1,24 +1,7 @@
 <?php
-echo "hallo wereld";
-
-echo 'this is a simple string';
-
-echo 'You can also have embedded newlines in
-strings this way as it is
-okay to do';
-
-// Outputs: Arnold once said: "I'll be back"
-echo 'Arnold once said: "I\'ll be back"';
-
-// Outputs: You deleted C:\*.*?
-echo 'You deleted C:\\*.*?';
-
-// Outputs: You deleted C:\*.*?
-echo 'You deleted C:\*.*?';
-
-// Outputs: This will not expand: \n a newline
-echo 'This will not expand: \n a newline';
-
-// Outputs: Variables do not $expand $either
-echo 'Variables do not $expand $either';
-?>
+$herhaling = "Tekst in een variable";
+echo $herhaling . PHP_EOL; 
+echo $herhaling . PHP_EOL;
+echo $herhaling . PHP_EOL;
+echo $herhaling . PHP_EOL;
+echo $herhaling . PHP_EOL;
